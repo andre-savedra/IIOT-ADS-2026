839a844c76b672b599771441e46d3cd16e3166c3
andre-savedra <andre-savedra@outlook.com>
2026-04-02 22:29:07 -0300

    ok

--- TURMA_A/9-IOT/backend/iiot_backend/src/app.module.ts
+++ TURMA_A/9-IOT/backend/iiot_backend/src/app.module.ts
@@ -2,10 +2,12 @@ import { Global, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MachineModule } from './machine/machine.module';
+import { SensorModule } from './sensor/sensor.module';
+import { DataModule } from './data/data.module';
 
 @Global()
 @Module({
-  imports: [MachineModule],
+  imports: [MachineModule, SensorModule, DataModule],
   controllers: [AppController],
   providers: [AppService],
   exports: [AppService],
